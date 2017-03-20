2336adb9b85f7744202754e4a3424a04e581abf9
Teichopsia <anthomelanous@hotmail.com>
2017-03-19 19:46:08 -0500

    Moved google maps script to js file

--- js/init.js
+++ js/init.js
@@ -1,10 +1,21 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav({
-        closeOnClick: true
+      closeOnClick: true
     });
-
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function initMap() {
+  var uluru = { lat: 8.988971, lng: -79.5243938 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
