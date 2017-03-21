398580c52960ec73e8b3ef8182b59630a0017b9e
Teichopsia <anthomelanous@hotmail.com>
2017-03-20 19:37:08 -0500

    refactored code and improved styling

--- js/init.js
+++ js/init.js
@@ -1,17 +1,20 @@
-(function ($) {
-  $(function () {
+$(document).ready(function () {
 
-    $('.button-collapse').sideNav({
-      closeOnClick: true
-    });
+  $('.button-collapse').sideNav({
+    closeOnClick: true
+  });
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  $('.carousel').carousel({
+    indicators: true,
+  });
 
-function initMap() {
+}); // end of document ready
+
+
+window.onload = function initMap() {
   var uluru = { lat: 8.988971, lng: -79.5243938 };
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
+    zoom: 18,
     center: uluru
   });
   var marker = new google.maps.Marker({
